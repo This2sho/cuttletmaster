eb89ad26d8889d7edee7610d14a3828bbf5e03a6
This2sho <zx00018@naver.com>
2022-09-11 23:57:04 +0900

    Add: 레스토랑에서 사용할 임베더블타입 주소 추가

---
+++ src/main/java/PorkCutlet/master/domain/Address.java
@@ -0,0 +1,23 @@
+package PorkCutlet.master.domain;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @Builder
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
