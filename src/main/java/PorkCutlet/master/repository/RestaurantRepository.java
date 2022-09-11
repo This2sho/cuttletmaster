efc42f48b7d6024cdb54609fe4a80885bc82c59e
This2sho <zx00018@naver.com>
2022-09-12 00:03:51 +0900

    Add: 레스토랑 레퍼지토리 추가
    data jpa와 querydsl을 사용을 위해 인터페이스 상속

---
+++ src/main/java/PorkCutlet/master/repository/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package PorkCutlet.master.repository;
+
+import PorkCutlet.master.domain.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long>, RestaurantRepositoryCustom {
+
+}
