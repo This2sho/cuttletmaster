2171be42dff1e10163446414fc4333367c46c4da
This2sho <zx00018@naver.com>
2022-09-12 00:04:21 +0900

    Test: Querydsl 잘 작동하는지 테스트

---
+++ src/test/java/PorkCutlet/master/repository/RestaurantRepositoryTest.java
@@ -0,0 +1,54 @@
+package PorkCutlet.master.repository;
+
+import PorkCutlet.master.domain.Address;
+import PorkCutlet.master.domain.Restaurant;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class RestaurantRepositoryTest {
+
+    @Autowired
+    RestaurantRepository restaurantRepository;
+
+    @Test
+    public void RestaurantCreateTest() {
+        //given
+        Address address = new Address("a city", "b street", "123");
+        Restaurant restaurant = new Restaurant("cocoa", address);
+        restaurantRepository.save(restaurant);
+        //when
+        Restaurant find = restaurantRepository.findById(restaurant.getId()).get();
+        //then
+        assertThat(restaurant.getId()).isEqualTo(find.getId());
+        assertThat(restaurant.getName()).isEqualTo(find.getName());
+        assertThat(restaurant.getAddress()).isEqualTo(find.getAddress());
+        assertThat(restaurant).isEqualTo(find);
+    }
+
+    @Test
+    public void RestaurantRepositoryQueryDslTest(){
+        //given
+        Address address = new Address("a city", "b street", "123");
+        Restaurant restaurant = new Restaurant("cocoa", address);
+        restaurantRepository.save(restaurant);
+        //when
+        Restaurant find = restaurantRepository.findRestaurantCustom().get(0);
+        //then
+        assertThat(restaurant).isEqualTo(find);
+        assertThat(restaurant.getId()).isEqualTo(find.getId());
+        assertThat(restaurant.getName()).isEqualTo(find.getName());
+        assertThat(restaurant.getAddress()).isEqualTo(find.getAddress());
+    }
+
+}
