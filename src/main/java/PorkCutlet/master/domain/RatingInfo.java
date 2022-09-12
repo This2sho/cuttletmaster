77ae0c170d12421421758d3fc7a1c33e2e2858c0
This2sho <zx00018@naver.com>
2022-09-12 22:32:30 +0900

    Add: 리뷰에 사용될 별점 정보 임베더블 타입으로 추가

---
+++ src/main/java/PorkCutlet/master/domain/RatingInfo.java
@@ -0,0 +1,25 @@
+package PorkCutlet.master.domain;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RatingInfo {
+    private Rating texture;
+    private Rating sauce;
+    private Rating side;
+    private Rating accessibility;
+    private double overall;
+
+    @Builder
+    public RatingInfo(Rating texture, Rating sauce, Rating side, Rating accessibility) {
+        this.texture = texture;
+        this.sauce = sauce;
+        this.side = side;
+        this.accessibility = accessibility;
+        this.overall = (texture.getValue() + sauce.getValue() + side.getValue() + accessibility.getValue()) / 5;
+    }
+}
