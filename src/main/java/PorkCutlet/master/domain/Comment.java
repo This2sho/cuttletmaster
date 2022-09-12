1cca4cc6f9d749abccf4d24c3a7588435b2dd8f0
This2sho <zx00018@naver.com>
2022-09-12 22:33:31 +0900

    Add: 댓글 엔티티 추가

---
+++ src/main/java/PorkCutlet/master/domain/Comment.java
@@ -0,0 +1,29 @@
+package PorkCutlet.master.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Comment extends BaseTimeEntity{
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+}
