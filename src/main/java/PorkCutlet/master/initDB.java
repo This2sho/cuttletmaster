0671a3b38afabeeb2cd798e9c43de2e070150d4f
This2sho <zx00018@naver.com>
2022-09-13 01:34:53 +0900

    Test: 테스트용 데이터 설정

---
+++ src/main/java/PorkCutlet/master/initDB.java
@@ -0,0 +1,45 @@
+package PorkCutlet.master;
+
+import PorkCutlet.master.domain.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+import static PorkCutlet.master.domain.Rating.*;
+
+@Component
+@RequiredArgsConstructor
+public class initDB {
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+
+        public void dbInit() {
+            char[] 성 = {'김', '박', '손'};
+            for (int i = 0; i < 3; i++) {
+                em.persist(new User(성[i] + "민수", "1234", "user" + i));
+            }
+
+            Rating[] 별점 = {ONE, TWO, THREE, FOUR, Five};
+            for (int i = 1; i < 5; i++) {
+                Restaurant restaurant = new Restaurant("식당" + i, new Address("부산", "부산 대학로", "147-" + i));
+                em.persist(restaurant);
+                em.persist(new Review("맛있다"+i, restaurant, new RatingInfo(별점[i], 별점[i], 별점[i], 별점[i])));
+            }
+        }
+    }
+
+}
