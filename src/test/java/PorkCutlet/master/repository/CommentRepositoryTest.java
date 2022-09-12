4855a8d217e00315cdb43371999a2daa41e665b7
This2sho <zx00018@naver.com>
2022-09-13 01:32:49 +0900

    Test: 댓글 레파지토리 기본 테스트

---
+++ src/test/java/PorkCutlet/master/repository/CommentRepositoryTest.java
@@ -0,0 +1,41 @@
+package PorkCutlet.master.repository;
+
+import PorkCutlet.master.domain.Comment;
+import PorkCutlet.master.domain.Review;
+import PorkCutlet.master.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class CommentRepositoryTest {
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Test
+    public void CommentRepositoryBasicTest() {
+        //given
+        User user1 = userRepository.findByNickName("user1").orElseThrow();
+        List<Review> all = reviewRepository.findAll();
+        Comment comment = new Comment(user1, all.get(0));
+        commentRepository.save(comment);
+        //when
+        Comment find = commentRepository.findById(comment.getId()).orElseThrow();
+        //then
+        assertThat(comment).isEqualTo(find);
+    }
+}
