42d759658d15ae4d2be92436c87a6f5d90320a82
This2sho <zx00018@naver.com>
2022-09-14 01:55:17 +0900

    Refactor: 회원 레파지토리 Jpa 레퍼지토리로 변경

--- src/main/java/PorkCutlet/master/repository/UserRepository.java
+++ src/main/java/PorkCutlet/master/repository/UserRepository.java
@@ -1,26 +1,13 @@
 package PorkCutlet.master.repository;
 
 import PorkCutlet.master.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Optional;
 
-@Repository
-public class UserRepository {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public Long save(User user) {
-        em.persist(user);
-        return user.getId();
-    }
-
-    public Optional<User> findById(Long id){
-        User user = em.find(User.class, id);
-        return Optional.ofNullable(user);
-    }
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLoginId(String loginId);
 }
