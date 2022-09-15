fdf7b5e86f410a13ed987dcd4170308bef8810bb
This2sho <zx00018@naver.com>
2022-09-15 23:29:07 +0900

    Refactor: User에서 UserDto로 바꾸는 로직 dto로 이동

--- src/main/java/PorkCutlet/master/controller/UserDto.java
+++ src/main/java/PorkCutlet/master/controller/UserDto.java
@@ -1,5 +1,6 @@
 package PorkCutlet.master.controller;
 
+import PorkCutlet.master.domain.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -14,4 +15,8 @@ public class UserDto {
     private String password;
     @NotEmpty
     private String nickName;
+
+    public static UserDto from(User user) {
+        return new UserDto(user.getLoginId(), user.getPassword(), user.getNickName());
+    }
 }
