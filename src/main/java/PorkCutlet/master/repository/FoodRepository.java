e8205cef47b9538283264bb01d337217a4ea667d
This2sho <zx00018@naver.com>
2022-09-23 23:29:26 +0900

    Chore: 파일 변경 및 필요없는 파일 삭제

--- src/main/java/PorkCutlet/master/repository/FoodRepository.java
+++
@@ -1,7 +0,0 @@
-package PorkCutlet.master.repository;
-
-import PorkCutlet.master.domain.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
