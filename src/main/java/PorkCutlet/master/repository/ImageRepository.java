4210620e0314a0107ea99a16360888a9e6667a1b
This2sho <zx00018@naver.com>
2022-09-26 18:56:56 +0900

    Add: 이미지 레파지토리 추가

---
+++ src/main/java/PorkCutlet/master/repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package PorkCutlet.master.repository;
+
+import PorkCutlet.master.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByUploadImageName(String imageName);
+}
