5c04cbd20e9c46327870e99b38440331db9cf332
This2sho <zx00018@naver.com>
2022-09-26 19:05:39 +0900

    Refactor: 회원 유형에 value 추가

--- src/main/java/PorkCutlet/master/domain/UserType.java
+++ src/main/java/PorkCutlet/master/domain/UserType.java
@@ -1,5 +1,10 @@
 package PorkCutlet.master.domain;
 
 public enum UserType {
-    COMMON, ADMIN
+    COMMON(0), ADMIN(1);
+    int value =0;
+
+    UserType(int value) {
+        this.value = value;
+    }
 }
