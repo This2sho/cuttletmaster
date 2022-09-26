b020a35af39b8bcbad22c334767c0329aa40c34e
This2sho <zx00018@naver.com>
2022-09-26 18:56:13 +0900

    Add: 이미지 엔티티 추가

---
+++ src/main/java/PorkCutlet/master/domain/Image.java
@@ -0,0 +1,34 @@
+package PorkCutlet.master.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Image {
+    @Id
+    @GeneratedValue
+    @Column(name = "image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    private String uploadImageName;
+    private String storeImageName;
+
+    @Builder
+    public Image(String uploadImageName, String storeImageName) {
+        this.uploadImageName = uploadImageName;
+        this.storeImageName = storeImageName;
+    }
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+}
