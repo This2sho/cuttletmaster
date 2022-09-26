025989e918f2fc23612fd21893301cdec9a430a3
This2sho <zx00018@naver.com>
2022-09-26 18:55:51 +0900

    Add: 홈화면에 필요한 리뷰 DTO 추가

---
+++ src/main/java/PorkCutlet/master/controller/HomeReviewDto.java
@@ -0,0 +1,16 @@
+package PorkCutlet.master.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class HomeReviewDto {
+    private String restaurantName;
+    private String forkCutletName;
+    private String roadAddress;
+    private String detailAddress;
+    private List<String> storeFileNames;
+}
