24e5dcb79d9a25992f8f48747e6e06e89a5646c9
This2sho <zx00018@naver.com>
2022-09-26 18:54:11 +0900

    Refactor: 돈가스 타입에 value 추가

--- src/main/java/PorkCutlet/master/domain/ForkCutletType.java
+++ src/main/java/PorkCutlet/master/domain/ForkCutletType.java
@@ -1,5 +1,11 @@
 package PorkCutlet.master.domain;
 
 public enum ForkCutletType {
-    KOREAN_STYLE, JAPANESE_STYLE
+    KOREAN_STYLE(0), JAPANESE_STYLE(1);
+
+    private int value;
+
+    ForkCutletType(int value) {
+        this.value = value;
+    }
 }
