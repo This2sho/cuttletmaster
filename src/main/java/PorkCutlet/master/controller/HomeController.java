e0b984ba8cabd7813e9d9c6b821d5ca0e907e678
This2sho <zx00018@naver.com>
2022-09-26 18:55:28 +0900

    Feat: 홈컨트롤러에 필요한 리뷰 받아오기 및 추천 기능 구현

--- src/main/java/PorkCutlet/master/controller/HomeController.java
+++ src/main/java/PorkCutlet/master/controller/HomeController.java
@@ -1,20 +1,70 @@
 package PorkCutlet.master.controller;
 
+import PorkCutlet.master.controller.login.Login;
+import PorkCutlet.master.domain.Address;
+import PorkCutlet.master.domain.Image;
+import PorkCutlet.master.domain.Restaurant;
+import PorkCutlet.master.domain.Review;
+import PorkCutlet.master.service.ReviewService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class HomeController {
-    @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) UserDto user, Model model) {
-        if (user == null) {
-            return "home";
-        }
+    private final ReviewService reviewService;
 
-        model.addAttribute("user", user);
-        return "loginHome";
+    @GetMapping("/")
+    public String home(@Login UserDto user, Model model) {
+        Review mostRecentReview = reviewService.getMostRecentReview();
+        model.addAttribute("mostRecentReview", reviewToHomeReviewDto(mostRecentReview));
+
+        List<HomeReviewDto> top6Reviews = reviewService.getTop6Reviews().stream()
+                .map(this::reviewToHomeReviewDto).collect(Collectors.toList());
+        model.addAttribute("top6Reviews", top6Reviews);
+
+        /**
+         * 
+         * 현재 추천 미구현으로 null 넣어놓음 구현 후 바꿔야 함
+         */
+        HomeReviewDto recommendReview = reviewToHomeReviewDto(reviewService.recommend(null));
+        model.addAttribute("recommendReview", recommendReview);
+        return "home";
     }
+
+    @PostMapping("/recommend")
+    public String recommend(@Login UserDto user, Model model) {
+        HomeReviewDto recommendReview = reviewToHomeReviewDto(reviewService.recommend(null));
+        model.addAttribute("recommendReview", recommendReview);
+        return "fragments/recommend";
+    }
+
+    public HomeReviewDto reviewToHomeReviewDto(Review review) {
+        Restaurant restaurant = review.getRestaurant();
+        Address address = restaurant.getAddress();
+        List<Image> images = review.getImages();
+
+        String restaurantName = restaurant.getName();
+        String forkCutletName = restaurant.getForkCutlet().getName();
+        String roadAddress = address.getRoadAddress();
+        String detailAddress = address.getDetailAddress();
+
+        List<String> storeFileNames = new ArrayList<>();
+        for (Image image : images) {
+            storeFileNames.add(image.getStoreImageName());
+        }
+        return new HomeReviewDto(restaurantName, forkCutletName, roadAddress, detailAddress, storeFileNames);
+    }
+
 
 }
