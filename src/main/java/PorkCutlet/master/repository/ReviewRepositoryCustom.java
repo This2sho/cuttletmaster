a409e12fc9b7bd59c27236d65f661a612475ffec
This2sho <zx00018@naver.com>
2022-09-27 21:09:15 +0900

    Feat: 리뷰 목록에서 N+1 문제로 인한 toOne관계 패치 조인 쿼리 구현

--- src/main/java/PorkCutlet/master/repository/ReviewRepositoryCustom.java
+++ src/main/java/PorkCutlet/master/repository/ReviewRepositoryCustom.java
@@ -1,14 +1,18 @@
 package PorkCutlet.master.repository;
 
 import PorkCutlet.master.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 
 public interface ReviewRepositoryCustom {
-    public List<Review> findReviewsFetchJoin();
+    List<Review> findReviewsFetchJoin();
 
-    public List<Review> findTop6Reviews();
+    Page<Review> findReviewsPageWithFetchJoin(Pageable pageable);
 
-    public Review findMostRecentReview();
+    List<Review> findTop6Reviews();
+
+    Review findMostRecentReview();
 }
--- src/main/java/PorkCutlet/master/repository/ReviewRepositoryImpl.java
+++ src/main/java/PorkCutlet/master/repository/ReviewRepositoryImpl.java
@@ -3,13 +3,16 @@ package PorkCutlet.master.repository;
 import PorkCutlet.master.domain.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static PorkCutlet.master.domain.QImage.*;
 import static PorkCutlet.master.domain.QRestaurant.*;
 import static PorkCutlet.master.domain.QReview.*;
+import static PorkCutlet.master.domain.QUser.*;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -20,8 +23,22 @@ public class ReviewRepositoryImpl implements ReviewRepositoryCustom{
     public List<Review> findReviewsFetchJoin() {
       return queryFactory.selectFrom(review)
                 .join(review.restaurant, restaurant).fetchJoin()
-                .join(review.images, image).fetchJoin()
+                .join(review.user, user).fetchJoin()
                 .fetch();
+    }
+
+    @Override
+    public Page<Review> findReviewsPageWithFetchJoin(Pageable pageable) {
+        long pageNum = pageable.getOffset() == 0 ? 0 : pageable.getOffset() - pageable.getPageSize();
+        List<Review> content = queryFactory.selectFrom(review)
+                .join(review.restaurant, restaurant).fetchJoin()
+                .join(review.user, user).fetchJoin()
+                .orderBy(review.createdDate.desc())
+                .offset(pageNum)
+                .limit(pageable.getPageSize())
+                .fetch();
+        int total = content.size();
+        return new PageImpl<> (content, pageable, total);
     }
 
     @Override
