d39df550868d130d27302438fff1bb279aecea75
This2sho <zx00018@naver.com>
2022-10-01 01:52:15 +0900

    Refactor: Review 에서 HomeReviewDto 로 변환하는 과정 변경
    기존 HomeController -> HomeReviewDto 로 변경

--- src/main/java/PorkCutlet/master/controller/HomeController.java
+++ src/main/java/PorkCutlet/master/controller/HomeController.java
@@ -3,9 +3,6 @@ package PorkCutlet.master.controller;
 import PorkCutlet.master.controller.dto.HomeReviewDto;
 import PorkCutlet.master.controller.dto.UserInfoDto;
 import PorkCutlet.master.controller.login.Login;
-import PorkCutlet.master.domain.Address;
-import PorkCutlet.master.domain.Image;
-import PorkCutlet.master.domain.Restaurant;
 import PorkCutlet.master.domain.Review;
 import PorkCutlet.master.service.ReviewService;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,44 +24,25 @@ public class HomeController {
     @GetMapping("/")
     public String home(@Login UserInfoDto user, Model model) {
         Review mostRecentReview = reviewService.getMostRecentReview();
-        model.addAttribute("mostRecentReview", reviewToHomeReviewDto(mostRecentReview));
+        model.addAttribute("mostRecentReview", HomeReviewDto.from(mostRecentReview));
 
         List<HomeReviewDto> top6Reviews = reviewService.getTop6Reviews().stream()
-                .map(this::reviewToHomeReviewDto).collect(Collectors.toList());
+                .map(HomeReviewDto::from).collect(Collectors.toList());
         model.addAttribute("top6Reviews", top6Reviews);
 
         /**
          *
          * 현재 추천 미구현으로 null 넣어놓음 구현 후 바꿔야 함
          */
-        HomeReviewDto recommendReview = reviewToHomeReviewDto(reviewService.recommend(null));
+        HomeReviewDto recommendReview = HomeReviewDto.from(reviewService.recommend(null));
         model.addAttribute("recommendReview", recommendReview);
         return "home";
     }
 
     @PostMapping("/recommend")
     public String recommend(@Login UserInfoDto user, Model model) {
-        HomeReviewDto recommendReview = reviewToHomeReviewDto(reviewService.recommend(null));
+        HomeReviewDto recommendReview = HomeReviewDto.from(reviewService.recommend(null));
         model.addAttribute("recommendReview", recommendReview);
         return "fragments/recommend";
     }
-
-    public HomeReviewDto reviewToHomeReviewDto(Review review) {
-        Restaurant restaurant = review.getRestaurant();
-        Address address = restaurant.getAddress();
-        List<Image> images = review.getImages();
-
-        String restaurantName = restaurant.getName();
-        String forkCutletName = restaurant.getForkCutlet().getName();
-        String roadAddress = address.getRoadAddress();
-        String detailAddress = address.getDetailAddress();
-
-        List<String> storeFileNames = new ArrayList<>();
-        for (Image image : images) {
-            storeFileNames.add(image.getStoreImageName());
-        }
-        return new HomeReviewDto(restaurantName, forkCutletName, roadAddress, detailAddress, storeFileNames);
-    }
-
-
 }
--- src/main/java/PorkCutlet/master/controller/dto/HomeReviewDto.java
+++ src/main/java/PorkCutlet/master/controller/dto/HomeReviewDto.java
@@ -1,8 +1,13 @@
 package PorkCutlet.master.controller.dto;
 
+import PorkCutlet.master.domain.Address;
+import PorkCutlet.master.domain.Image;
+import PorkCutlet.master.domain.Restaurant;
+import PorkCutlet.master.domain.Review;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -13,4 +18,21 @@ public class HomeReviewDto {
     private String roadAddress;
     private String detailAddress;
     private List<String> storeFileNames;
+
+    public static HomeReviewDto from(Review review) {
+        Restaurant restaurant = review.getRestaurant();
+        Address address = restaurant.getAddress();
+        List<Image> images = review.getImages();
+
+        String restaurantName = restaurant.getName();
+        String forkCutletName = restaurant.getForkCutlet().getName();
+        String roadAddress = address.getRoadAddress();
+        String detailAddress = address.getDetailAddress();
+
+        List<String> storeFileNames = new ArrayList<>();
+        for (Image image : images) {
+            storeFileNames.add(image.getStoreImageName());
+        }
+        return new HomeReviewDto(restaurantName, forkCutletName, roadAddress, detailAddress, storeFileNames);
+    }
 }
