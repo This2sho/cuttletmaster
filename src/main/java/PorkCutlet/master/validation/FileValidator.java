ca4019a88fceb81660c6a9b9d9d4e54d5735fba0
This2sho <zx00018@naver.com>
2022-10-12 22:38:44 +0900

    Fix: 파일 업로드 유효성 검사 오류 수정
    List로 MultipartFile을 감싸면 List의 초기화 크기는 1..

--- src/main/java/PorkCutlet/master/validation/FileValidator.java
+++ src/main/java/PorkCutlet/master/validation/FileValidator.java
@@ -1,12 +1,15 @@
 package PorkCutlet.master.validation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Component
+@Slf4j
 public class FileValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
@@ -15,9 +18,10 @@ public class FileValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        List file = (List) target;
-        if (file.isEmpty() || file.size() < 1) {
-            errors.rejectValue("imageFile","emptyFile", "파일을 업로드해주세요.");
+        List<MultipartFile> file = (List<MultipartFile>) target;
+
+        if (file.get(0).isEmpty()) {
+            errors.rejectValue("imageFiles", "emptyFile", "파일을 업로드해주세요.");
         }
     }
 }
