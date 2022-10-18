223f0e3b83f9bc031aeda68da83b5bd740022b96
This2sho <zx00018@naver.com>
2022-10-18 20:04:10 +0900

    Refactor: 리뷰폼(DTO) 수정용 필드 추가

--- src/main/java/PorkCutlet/master/controller/dto/ReviewForm.java
+++ src/main/java/PorkCutlet/master/controller/dto/ReviewForm.java
@@ -2,6 +2,7 @@ package PorkCutlet.master.controller.dto;
 
 import PorkCutlet.master.domain.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,10 +25,9 @@ public class ReviewForm {
 
     @NotEmpty(message = "주소를 입력해주세요.")
     private String roadAddress;
-    private String detailAddress;
 
-    private List<String> preImageFiles;
-    private List<MultipartFile> imageFiles;
+    @Builder.Default
+    private String detailAddress = "";
 
     @NotEmpty(message = "한줄평을 입력해주세요.")
     private String oneSentence;
@@ -55,12 +55,22 @@ public class ReviewForm {
     @NotNull(message = "평점을 입력해주세요.")
     private Integer accessibility;
 
+    private List<String> preImages;
+
+    private List<String> deleteImages;
+
+    private List<MultipartFile> imageFiles;
+
     public Address makeAddress() {
         return new Address(roadAddress, detailAddress);
     }
 
     public ForkCutlet makeForkCutlet() {
-        return new ForkCutlet(forkCutletName, ForkCutletType.values()[forkCutletType]);
+        return new ForkCutlet(forkCutletName, makeForkCutletType());
+    }
+
+    public ForkCutletType makeForkCutletType() {
+        return ForkCutletType.values()[forkCutletType];
     }
 
     public Restaurant makeRestaurant() {
@@ -70,12 +80,12 @@ public class ReviewForm {
     public RatingInfo makeRatingInfo() {
         int[] ratings = new int[] {texture, sauce, side, accessibility};
         Rating[] values = Rating.values();
-        return new RatingInfo(values[ratings[0]-1], values[ratings[1]-1], values[ratings[2]-1], values[ratings[3]-1]);
+        return new RatingInfo(values[ratings[0]], values[ratings[1]], values[ratings[2]], values[ratings[3]]);
     }
 
     public static ReviewForm from(Review review) {
         return new ReviewForm(review.getRestaurant().getName(), review.getRestaurant().getForkCutlet().getName(), review.getRestaurant().getForkCutlet().getForkCutletType().getValue(),
-                review.getRestaurant().getAddress().getRoadAddress(), review.getRestaurant().getAddress().getDetailAddress(), review.getImages().stream().map(i -> i.getStoreImageName()).collect(Collectors.toList()), new ArrayList<>(), review.getOneSentence(), review.getContent(),
-                review.getRatingInfo().getTexture().getValue(), review.getRatingInfo().getSauce().getValue(), review.getRatingInfo().getSide().getValue(), review.getRatingInfo().getAccessibility().getValue());
+                review.getRestaurant().getAddress().getRoadAddress(), review.getRestaurant().getAddress().getDetailAddress(), review.getOneSentence(), review.getContent(),
+                review.getRatingInfo().getTexture().getValue(), review.getRatingInfo().getSauce().getValue(), review.getRatingInfo().getSide().getValue(), review.getRatingInfo().getAccessibility().getValue(), review.getImages().stream().map(Image::getStoreImageName).collect(Collectors.toList()),new ArrayList<>(), null);
     }
 }
