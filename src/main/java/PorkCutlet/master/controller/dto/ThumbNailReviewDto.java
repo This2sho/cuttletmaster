5e94665164ffaa89b6514a27a4271bdbb9c77d23
This2sho <zx00018@naver.com>
2022-10-23 03:40:10 +0900

    Refactor: DTO에 돈가스 타입 추가

--- src/main/java/PorkCutlet/master/controller/dto/ThumbNailReviewDto.java
+++ src/main/java/PorkCutlet/master/controller/dto/ThumbNailReviewDto.java
@@ -12,12 +12,14 @@ public class ThumbNailReviewDto {
     private String storeImageName;
     private String restaurantName;
     private String forkCutletName;
+    private String forkCutletType;
     private Double overallRating;
     private String createdDate;
     private String userNickName;
 
     public static ThumbNailReviewDto from(Review review) {
-        return new ThumbNailReviewDto(review.getId(),review.getImages().get(0).getStoreImageName(), review.getRestaurant().getName(), review.getRestaurant().getForkCutlet().getName(), review.getRatingInfo().getOverall(),
-                review.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")), review.getUser().getNickName());
+        return new ThumbNailReviewDto(review.getId(),review.getImages().get(0).getStoreImageName(),
+                review.getRestaurant().getName(), review.getRestaurant().getForkCutlet().getName(), review.getRestaurant().getForkCutlet().getForkCutletType().getName(),
+                review.getRatingInfo().getOverall(), review.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")), review.getUser().getNickName());
     }
 }
