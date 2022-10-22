c223138896cb6b7032dcb573a022435f7ee026ea
This2sho <zx00018@naver.com>
2022-10-23 03:34:35 +0900

    Test: 리뷰 기능 구현 테스트
    이전에 커밋 못한것들..

---
+++ src/test/java/PorkCutlet/master/domain/ReviewTest.java
@@ -0,0 +1,116 @@
+package PorkCutlet.master.domain;
+
+import PorkCutlet.master.repository.ImageRepository;
+import PorkCutlet.master.repository.RestaurantRepository;
+import PorkCutlet.master.repository.ReviewRepository;
+import PorkCutlet.master.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static PorkCutlet.master.domain.Rating.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class ReviewTest {
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Autowired
+    ImageRepository imageRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RestaurantRepository restaurantRepository;
+    static final Rating[] 별점 = {ONE, TWO, THREE, FOUR, FIVE};
+
+    @Test
+    public void ReviewImageTest() throws Exception {
+        //given
+        Image image = new Image("upload", "store");
+        List<Image> images = List.of(image);
+        Review review = new Review(userRepository.findByLoginId("user1").get(), restaurantRepository.findByName("식당1").get(), images, "it's sososo gooood","good", new RatingInfo(별점[2], 별점[2], 별점[2], 별점[2]));
+        //when
+        Review imageReview = image.getReview();
+        Image reviewImage = review.getImages().get(0);
+        //then
+        assertThat(imageReview).isEqualTo(review);
+        assertThat(reviewImage).isEqualTo(image);
+    }
+
+    @Test
+    @Rollback(value = false)
+    /**
+     * image와 리뷰는 cascade all 관계라 image를 persist하지 않아도 됨
+     * **/
+    public void 이미지DB생성테스트() throws Exception {
+        //given
+        Image image = new Image("upload", "store");
+        List<Image> images = List.of(image);
+        Review review = new Review(userRepository.findByLoginId("user1").get(), restaurantRepository.findByName("식당1").get(), images, "it's sososo gooood", "good", new RatingInfo(별점[2], 별점[2], 별점[2], 별점[2]));
+        reviewRepository.save(review);
+        //when
+        Review imageReview = image.getReview();
+        Image reviewImage = review.getImages().get(0);
+        //then
+        assertThat(imageReview).isEqualTo(review);
+        assertThat(reviewImage).isEqualTo(image);
+    }
+
+    @Test
+    /**
+     * 리뷰를 삭제했을 때 이미지는 삭제되야되지만,
+     * 이미지를 삭제했을 때 리뷰는 삭제되면 안됨
+     * **/
+    public void 고아객체_이미지_삭제테스트() throws Exception {
+        //given
+        Image image = new Image("upload", "store");
+        List<Image> images = List.of(image);
+        Review review = new Review(userRepository.findByLoginId("user1").get(), restaurantRepository.findByName("식당1").get(), images, "it's sososo gooood", "good", new RatingInfo(별점[2], 별점[2], 별점[2], 별점[2]));
+        reviewRepository.save(review);
+        //when
+        imageRepository.delete(image);
+        //then
+        Optional<Review> findReview = reviewRepository.findById(review.getId());
+        assertThat(review).isEqualTo(findReview.get());
+    }
+
+    @Test
+    /**
+     * 리뷰를 삭제했을 때 이미지는 삭제되야되지만,
+     * 이미지를 삭제했을 때 리뷰는 삭제되면 안됨
+     * **/
+    public void 고아객체_리뷰_삭제테스트() throws Exception {
+        //given
+        Image image = new Image("upload", "store");
+        List<Image> images = List.of(image);
+        Review review = new Review(userRepository.findByLoginId("user1").get(), restaurantRepository.findByName("식당1").get(), images, "it's sososo gooood", "good", new RatingInfo(별점[2], 별점[2], 별점[2], 별점[2]));
+        reviewRepository.save(review);
+        //when
+        reviewRepository.delete(review);
+        //then
+        Optional<Image> findImage = imageRepository.findById(image.getId());
+        assertThat(findImage).isEmpty();
+    }
+    
+    @Test
+    public void 지연로딩_리뷰_프록시_테스트() throws Exception {
+        //given
+        Review findReview = reviewRepository.findByRestaurant_Name("식당1").get();
+        // select 쿼리 2개 나감
+    }
+
+    @Test
+    public void 지연로딩_이미지_프록시_테스트() throws Exception {
+        //given
+        Image image = imageRepository.findByUploadImageName("upload1").get();
+        //select 쿼리 1개 나감.
+        assertThat(image.getReview().getClass()).isNotEqualTo(Review.class);
+    }
+
+}
--- src/test/java/PorkCutlet/master/repository/ReviewRepositoryTest.java
+++ src/test/java/PorkCutlet/master/repository/ReviewRepositoryTest.java
@@ -1,15 +1,25 @@
 package PorkCutlet.master.repository;
 
 import PorkCutlet.master.domain.*;
+import PorkCutlet.master.service.ReviewService;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
+import static PorkCutlet.master.domain.QForkCutlet.forkCutlet;
+import static PorkCutlet.master.domain.QLike.like;
+import static PorkCutlet.master.domain.QRestaurant.restaurant;
+import static PorkCutlet.master.domain.QReview.review;
+import static PorkCutlet.master.domain.QUser.user;
 import static PorkCutlet.master.domain.Rating.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -20,12 +30,22 @@ class ReviewRepositoryTest {
     ReviewRepository reviewRepository;
     @Autowired
     RestaurantRepository restaurantRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    LikeRepository likeRepository;
+    @Autowired
+    JPAQueryFactory queryFactory;
+
+    @Autowired
+    ReviewService reviewService;
 
     @Test
     public void ReviewRepositoryTest() {
         //given
-        Restaurant restaurant = restaurantRepository.findByName("식당1").orElseThrow();
-        Review review = new Review("마싯쿤", restaurant, new RatingInfo(Five, THREE, FOUR, ONE));
+        Image image = new Image("upload", "store");
+        List<Image> images = List.of(image);
+        Review review = new Review(userRepository.findByLoginId("user1").get(), restaurantRepository.findByName("식당1").get(),images, "it's sososo gooood","마싯쿤", new RatingInfo(FIVE, THREE, FOUR, ONE));
         reviewRepository.save(review);
         //when
         Review find = reviewRepository.findById(review.getId()).orElseThrow();
@@ -36,15 +56,129 @@ class ReviewRepositoryTest {
 
     }
 
-//    @Test
-//    public void ProxyTest() {
-//        //given
-//        Review review = reviewRepository.findByContent("맛있다1").orElseThrow();
-//        //when
-//        System.out.println(review.getRestaurant().getClass());
-//        //then
-//
-////        Restaurant restaurant = restaurantRepository.findByName("식당1").orElseThrow();
-////        System.out.println(restaurant);
-//    }
+    @Test
+    public void 리뷰_레스토랑_한방쿼리_테스트() throws Exception {
+        //given
+        List<Review> reviewsFetchJoin = reviewRepository.findReviewsWithFetchJoin();
+        Review randomReview = reviewsFetchJoin.stream().findAny().orElseThrow();
+        //when
+        System.out.println("reviewsFetchJoin.size() = " + reviewsFetchJoin.size());
+        System.out.println(randomReview.getRestaurant().getName());
+        System.out.println(randomReview.getImages().get(0).getStoreImageName());
+        System.out.println(randomReview.getRestaurant().getForkCutlet().getName());
+        //then
+    }
+
+    @Test
+    public void 제일_높은_리뷰_6개_찾기() throws Exception {
+        //given
+        List<Review> reviews = reviewRepository.findTop6Reviews();
+        //when
+        for (Review review : reviews) {
+            System.out.println("review = " + review.getRestaurant().getName());
+        }
+        //then
+
+    }
+
+    @Test
+    public void 제일_최신_리뷰찾기() throws Exception {
+        //given
+        Review mostRecentReview = reviewRepository.findMostRecentReview();
+        //when
+        System.out.println("mostRecentReview = " + mostRecentReview.getRestaurant().getName());
+        System.out.println("created = " + mostRecentReview.getCreatedDate());
+        //then
+
+    }
+
+    @Test
+    public void 더좋아하는돈가스타입() throws Exception {
+        //given
+        List<User> users = userRepository.findAll();
+        User user0 = users.get(0);
+        List<Review> reviews = reviewRepository.findAll();
+        reviews.get(0).getRestaurant().getForkCutlet().update("aaa", ForkCutletType.JAPANESE_STYLE);
+        for (int i = 0; i < 3; i++) {
+            Like like1 = new Like(user0, reviews.get(i));
+            likeRepository.save(like1);
+        }
+
+        Long userId = user0.getId();
+
+        List<Tuple> fetch = queryFactory.select(forkCutlet.forkCutletType, forkCutlet.count())
+                .from(like)
+                .join(like.review, review)
+                .join(like.user, user)
+                .join(review.restaurant, restaurant)
+                .join(restaurant.forkCutlet, forkCutlet)
+                .where(user.id.eq(userId))
+                .groupBy(forkCutlet.forkCutletType)
+                .fetch();
+
+        for (Tuple tuple : fetch) {
+            System.out.print(tuple.get(0, ForkCutletType.class) + " : ");
+            System.out.println(tuple.get(1, Long.class));
+        }
+    }
+
+    @Test
+    public void 원하는_돈가스타입_리뷰_가져오기_테스트() throws Exception {
+        //given
+        List<User> users = userRepository.findAll();
+        User user0 = users.get(0);
+        List<Review> reviews = reviewRepository.findAll();
+        reviews.get(0).getRestaurant().getForkCutlet().update("aaa", ForkCutletType.JAPANESE_STYLE);
+        for (int i = 0; i < 3; i++) {
+            Like like1 = new Like(user0, reviews.get(i));
+            likeRepository.save(like1);
+        }
+
+        ForkCutletType forkCutletType = ForkCutletType.JAPANESE_STYLE;
+
+        List<Review> fetch = queryFactory.selectFrom(review)
+                .join(review.restaurant, restaurant).fetchJoin()
+                .join(review.user, user).fetchJoin()
+                .where(restaurant.forkCutlet.forkCutletType.eq(forkCutletType))
+                .fetch();
+
+        for (Review review : fetch) {
+            System.out.println(review.getRestaurant().getName());
+            System.out.println(review.getRestaurant().getForkCutlet().getForkCutletType());
+        }
+    }
+
+    @Test
+    public void 돈가스_추천() throws Exception {
+        List<User> users = userRepository.findAll();
+        User user0 = users.get(0);
+        List<Review> reviews = reviewRepository.findAll();
+        reviews.get(0).getRestaurant().getForkCutlet().update("aaa", ForkCutletType.JAPANESE_STYLE);
+
+        for (int i = 0; i < 3; i++) {
+            Like like1 = new Like(user0, reviews.get(i));
+            likeRepository.save(like1);
+        }
+
+        Long userId = user0.getId();
+
+        List<Review> result = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Review recommendReview = reviewService.getRecommendReview(userId);
+            result.add(recommendReview);
+        }
+
+        int KOREAN_STYLE = 0;
+        int JAPANESE_STYLE = 0;
+
+        for (Review review : result) {
+            if (review.getRestaurant().getForkCutlet().getForkCutletType() == ForkCutletType.KOREAN_STYLE) {
+                KOREAN_STYLE++;
+            }else{
+                JAPANESE_STYLE++;
+            }
+        }
+
+        System.out.println("KOREAN_STYLE : " + KOREAN_STYLE + ", JAPANESE_STYLE : " + JAPANESE_STYLE);
+    }
 }
