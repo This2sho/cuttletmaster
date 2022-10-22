0910f10fe94f3af95e925eb8c14ab3704bf47d0e
This2sho <zx00018@naver.com>
2022-10-23 02:50:45 +0900

    Chore: import 최적화

--- src/main/java/PorkCutlet/master/controller/ImageController.java
+++ src/main/java/PorkCutlet/master/controller/ImageController.java
@@ -2,11 +2,13 @@ package PorkCutlet.master.controller;
 
 import PorkCutlet.master.ImageUtils;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.net.MalformedURLException;
 
